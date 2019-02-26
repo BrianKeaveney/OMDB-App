0494ea67203da424fc0951c930802caabfcae3d4
Brian Keaveney <S00179227@mail.itsligo.ie>
2019-02-26 16:14:26 +0000

    first

---
+++ src/app/omdb-api.service.ts
@@ -0,0 +1,31 @@
+import { IOMDBResponse } from './iomdbresponse';
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {catchError, tap} from 'rxjs/operators';
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OmdbApiService {
+
+  private _siteURL = 'http://www.omdbapi.com/?i=tt3896198';
+  private _key = '&apikey=6d4ab2bc';
+
+  constructor(private _http: HttpClient) { }
+
+  getMovieData(movieName): Observable<IOMDBResponse> {
+    console.log("URL: "+this._siteURL+movieName+this._key);
+    return this._http.get<IOMDBResponse>(this._siteURL+movieName+this._key).pipe(
+      tap(data => console.log("All " + JSON.stringify(data))),
+      catchError(this.handleError));   
+  }
+
+  private handleError(err: HttpErrorResponse) {
+    console.log('OmdbApiService: ' + err.message);
+    return Observable.throw(err.message);
+  }
+}
+
